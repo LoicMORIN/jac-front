22d1dbb81431990f8fc51d1df3013341281755d1
LoicMORIN <lmorin94@live.fr>
2022-03-13 22:19:19 +0100

    2

--- src/Route/AuthNavigator.tsx
+++ src/Route/AuthNavigator.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useAuth } from "../Context/AuthContext";
 import LoginView from "../Views/LoginView";
+import ConnexionNavigator from "./Connexion";
 import TabNavigator from "./TabNavigator";
 
 function AuthNavigator(): any {
   const { isAuthentificated }: any = useAuth();
   if (!isAuthentificated) {
-    return <LoginView />;
+    return <ConnexionNavigator />;
   } else {
     return <TabNavigator />;
   }
