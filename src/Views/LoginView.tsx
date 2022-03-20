f8bcce96d118a5f21972ac465ee6f28b7ec1fa67
LoicMORIN <lmorin94@live.fr>
2022-03-20 22:49:01 +0100

    image conexion

--- src/Views/LoginView.tsx
+++ src/Views/LoginView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Button, } from 'react-native';
+import {Text, View, Button, Image} from 'react-native';
 import { useAuth } from "../Context/AuthContext";
 import Style from '../../Style';
 
@@ -9,7 +9,11 @@ function LoginView({navigation}:any) {
     const { login } = useAuth();
     return (
       <View style={Style.container}>
-        <Text>Ecran de connexion</Text>
+        <Image source={require('../../assets/logo_UNICEF.png')} />
+        <Text>Connexion à mon compte UNICEF</Text>
+        <Image source={require('../../assets/@.png')} />
+        <Image source={require('../../assets/fermer-a-cle.png')} />
+        <Image source={require('../../assets/croise-les-yeux.png')} />
         <Button title="Se connecter" onPress={() => login()} />
         <Text onPress={() => navigation.navigate('ForgetPassword')} style={Style.text_button}> Mot de passe oublié ? </Text>
         <Text> Pas encore inscrit ? </Text><Text onPress={() => navigation.navigate('SignUp')} style={Style.text_button}> S'inscrire </Text>
