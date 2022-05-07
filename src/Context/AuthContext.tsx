3215e7cf81b3e81f81a28e617dbc70b5421b9fb6
LoicMORIN <lmorin94@live.fr>
2022-05-07 11:39:51 +0200

    Back 1

--- src/Context/AuthContext.tsx
+++ src/Context/AuthContext.tsx
@@ -1,12 +1,31 @@
-import React, { useState, useContext, createContext } from "react";
+import React, { useState, useContext, useEffect, createContext } from "react";
+
+
 
 const AuthContext = createContext({
   isAuthentificated: false,
-  login: () => {},
+  login: (email_login:string,password_login:string) => {},
 });
 function AuthProvider({ children }: any) {
   const [isAuthentificated, setIsAuthentificated] = useState(false);
-  function login() {
+  const [data, setData] = useState([]);
+  function login(email_login:string,password_login:string) {
+    const requestOption = {
+      method: "GET",
+      headers:{'Content-Type':'application/json'},
+    }
+    const getUsers = async () => {
+      try {
+       const response = await fetch('localhost:3000/users');
+       const json = await response.json();
+       setData(json);
+     } catch (error) {
+       console.error(error);
+     }
+    }
+    return ([data.email, data.password]);
+    
+    
     setIsAuthentificated(true);
   }
   return (
